b284e6393c30ae304260141689a88d66bc2b4818
Purushoth Kumar <purushothkumar1472@gmail.com>
2020-02-03 09:59:45 +0530

    @sec commit

---
+++ app/src/main/java/in/apexcoders/myapplication/AidService.java
@@ -0,0 +1,56 @@
+package in.apexcoders.myapplication;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+
+public class AidService extends Service implements SensorEventListener {
+    Sensor sensor;
+    SensorManager sensorManager;
+
+    public AidService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //int a = 65;
+        //char i = (char)a;
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
---
+++ app/src/main/java/in/apexcoders/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package in.apexcoders.myapplication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button serviceButton;
+    SharedPreferences pref;
+    Boolean isOn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        serviceButton = findViewById(R.id.serviceButton);
+        pref = getSharedPreferences("MyPref", MODE_PRIVATE);
+        isOn = pref.getBoolean("Service", false);
+        if (isOn) {
+            serviceButton.setText("Stop Service");
+        } else {
+            serviceButton.setText("Start Service");
+        }
+
+        serviceButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.serviceButton) {
+            Intent intent = new Intent(MainActivity.this, AidService.class);
+
+            if (isOn) {
+                //stop the service
+                stopService(intent);
+                isOn = false;
+                serviceButton.setText("Start Service");
+            } else {
+                //start the service
+                startService(intent);
+                isOn = true;
+                serviceButton.setText("Stop Service");
+
+            }
+        }
+    }
